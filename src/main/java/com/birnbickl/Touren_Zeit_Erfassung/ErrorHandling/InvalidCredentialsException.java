7f98c04174086b0d1503cc11f7ff00b916921b50
JBirnbickl93 <jo.birnbickl@web.de>
2025-05-13 21:06:01 +0200

    Package Änderung die 2.
    und hoffentlich letzte.

--- src/main/java/com/birnbickl/Touren_Zeit_Erfassung/ErrorHandling/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.birnbickl.Touren_Zeit_Erfassung.ErrorHandling;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
